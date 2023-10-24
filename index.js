9e8ab30fe91b6f8854f8ab94c8f855e3454d4efb
Goomes-2 <guilhermegdesouza@hotmail.com>
2023-10-24 01:09:09 -0300

    projeto do felipao

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Olá Mundo")
+let nome = "GOMES";
+let xp = 8600;
+
+if (xp <= 1000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Ferro");
+} else if (xp <= 2000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Bronze");
+} else if (xp <= 5000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Prata");
+} else if (xp <= 7000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Ouro");
+} else if (xp <= 8000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Platina");
+} else if (xp <= 9000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Ascendente");
+} else if (xp <= 10000) {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Imortal");
+} else {
+  console.log(nome + " você está no nível de XP " + xp + " que equivale ao Radiante");
+}
+
